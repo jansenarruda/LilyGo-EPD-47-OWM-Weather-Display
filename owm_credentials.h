f487ed0a97efdf1add25e1a05aa2c15eb8a01bf5
G6EJD <d.l.bird@sky.com>
2021-04-14 11:58:27 +0100

    Add files via upload

---
+++ owm_credentials.h
@@ -0,0 +1,16 @@
+const bool DebugDisplayUpdate = false;
+// Change to your WiFi credentials
+const char* ssid     = "ssid";
+const char* password = "password";
+String apikey        = "key";
+const char server[]  = "api.openweathermap.org";
+String City             = "Bath";   
+String Latitude         = "52";              
+String Longitude        = "2"; 
+String Language         = "EN";                        
+String Hemisphere       = "north";                        
+String Units            = "M";                            
+const char* Timezone    = "GMT0BST,M3.5.0/01,M10.5.0/02";  
+const char* ntpServer   = "0.uk.pool.ntp.org";
+int   gmtOffset_sec     = 0;
+int   daylightOffset_sec = 3600;
